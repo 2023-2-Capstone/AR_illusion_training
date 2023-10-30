ee200aabe1338374d57a5c30358732dbb4d6959b
Ohbrent <57317183+ohbrent@users.noreply.github.com>
2023-10-30 12:58:47 +0900

    ARSceneManager추가, 여기에 코드 추가.
    이 상태에서는 다른 씬으로 전환하는 코드가 임의로 추가되었음

---
+++ AR_illusion_training/Assets/UI/ARSceneManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ARSceneManager : MonoBehaviour
+{
+    /*
+    씬을 전환하고 싶을 때 사용
+    
+    void GotoMain(){
+        GotoScene("Main");
+    }
+    void GotoScene(string sceneName){
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+    */
+}
