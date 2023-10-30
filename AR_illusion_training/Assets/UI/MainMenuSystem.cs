460c13ab8fd8312964d552e8dcc0602cb938a7f1
Ohbrent <57317183+ohbrent@users.noreply.github.com>
2023-10-30 18:07:08 +0900

    버튼 기능 추가
    시작 버튼과 종료 버튼이 있으며, 종료 버튼을 누를 시 앱이 종료함

---
+++ AR_illusion_training/Assets/UI/MainMenuSystem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickedStart(){
+        Debug.Log("게임 시작");
+    }
+    public void OnClickedQuit(){
+        Debug.Log("종료");
+    //유니티 에디터에서는 Application.Quit()가 동작하지 않기 때문에 다음과 같이 처리
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
