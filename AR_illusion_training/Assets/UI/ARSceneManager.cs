8ca134b6343c3f866f428288e39563f2abcecb96
Ohbrent <57317183+ohbrent@users.noreply.github.com>
2023-10-30 13:14:25 +0900

    가속계 가중치 변경 확인용 버튼 생성

--- AR_illusion_training/Assets/UI/ARSceneManager.cs
+++ AR_illusion_training/Assets/UI/ARSceneManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,12 +8,20 @@ public class ARSceneManager : MonoBehaviour
 {
     /*
     씬을 전환하고 싶을 때 사용
-    
-    void GotoMain(){
+
+    public void GotoMain(){
         GotoScene("Main");
     }
-    void GotoScene(string sceneName){
+    public void GotoScene(string sceneName){
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
     */
+
+    //ref은 c++의 &이다.
+    public void AddVelocity(ref float vel){
+        vel += 1;
+    }
+    public void SubVelocity(ref float vel){
+        vel -= 1;
+    }
 }
