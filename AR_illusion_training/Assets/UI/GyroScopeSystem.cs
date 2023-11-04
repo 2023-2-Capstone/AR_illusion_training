03798ec0e0a95e799f9f58cca7aefb64c9ff8233
Ohbrent <57317183+ohbrent@users.noreply.github.com>
2023-11-04 15:21:11 +0900

    텍스트 2개 추가
    아직 기능 없음

---
+++ AR_illusion_training/Assets/UI/GyroScopeSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroScopeSystem : MonoBehaviour
+{
+    private Gyroscope Gyro;
+    private Vector3 angleGryo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angleGryo
+    }
+}
