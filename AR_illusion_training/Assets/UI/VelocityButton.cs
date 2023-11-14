bcace0309ebaa3be4f77b29e04e97d0aa7d2dadb
Lee SeJin <lsj8706@naver.com>
2023-11-14 19:27:58 +0900

    Velocity Start 시점에 가속도 측정 시작

--- AR_illusion_training/Assets/UI/VelocityButton.cs
+++ AR_illusion_training/Assets/UI/VelocityButton.cs
@@ -5,15 +5,23 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class VelocityButton : MonoBehaviour
 {
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern void _startTracking(string objectName);
+#endif
+
     public TextMeshProUGUI VelText;
     float Velocity;
+    bool isTracking = false;
 
     void Start(){
         Velocity = 0;
         UpdateVelocityText();
+        StartSpeedTracking();
     }
 
     //ref은 c++의 &이다.
@@ -28,5 +36,25 @@ public class VelocityButton : MonoBehaviour
 
     private void UpdateVelocityText() {
         VelText.text = Velocity.ToString(); // Velocity 값을 문자열로 변환하여 표시
+    }
+
+    public void StartSpeedTracking()
+    {
+        if (this.isTracking == false)
+        {
+            this.isTracking = true;
+#if UNITY_IOS
+            Debug.Log($"GameObjectName: {gameObject.name}");
+            _startTracking(gameObject.name);
+#else
+            Debug.Log("No iOS Device Found");
+#endif
+        }
+
+    }
+
+    public void SpeedCallBackMethod(string speed)
+    {
+        Debug.Log($"유니티에서 받은 스피드: {speed}");
     }
 }
