b0ee573b4c7289852d4f7d0204a11849f3636eb4
Lee SeJin <lsj8706@naver.com>
2023-11-14 17:54:16 +0900

    iOS 플러그인 추가

---
+++ AR_illusion_training/Assets/Plugins/iOS/UnityPlugin/Source/UnityPlugin-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  UnityPlugin-Bridging-Header.h
+//  UnityPlugin
+//
+//  Created by sejin on 2023/10/25.
+//
+
+#ifndef UnityPlugin_Bridging_Header_h
+#define UnityPlugin_Bridging_Header_h
+#import "UnityInterface.h"
+
+#endif /* UnityPlugin_Bridging_Header_h */
---
+++ AR_illusion_training/Assets/Plugins/iOS/UnityPlugin/Source/UnityPluginBridge.mm
@@ -0,0 +1,37 @@
+//
+//  UnityPluginBridge.m
+//  UnityPlugin
+//
+//  Created by sejin on 2023/10/25.
+//
+
+#import <Foundation/Foundation.h>
+#include "UnityFramework/UnityFramework-Swift.h"
+
+extern "C" {
+
+#pragma mark - Functions
+
+    int _addTwoNumberInIOS(int a , int b) {
+       
+        int result = [[UnityPlugin shared] AddTwoNumberWithA:(a) b:(b)];
+        return result;
+    }
+
+    const char* _getString(const char* name) {
+        const char* result = (const char*)[[[UnityPlugin shared] getStringWithName:[NSString stringWithUTF8String:name]] UTF8String];
+        
+        char* ptrReturnValue = (char*) malloc(strlen(result) + 1);
+        strcpy(ptrReturnValue, result);
+
+        return ptrReturnValue;
+    }
+
+    void FreePointer(char* ptr) {
+       free(ptr);
+    }
+
+    void _startTracking(const char* objectName) {
+        [[UnityPlugin shared] startTrackingWithObjectName:[NSString stringWithUTF8String:objectName]];
+    }
+}
